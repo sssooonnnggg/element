aa99fe64b668e70ddffd7ea9a9bc9ba2de236abc
shenzuomin <shenzuomin@7fgame.com>
2018-09-11 17:31:06 +0800

    确认框按钮换位置

--- packages/message-box/src/main.vue
+++ packages/message-box/src/main.vue
@@ -21,19 +21,19 @@
         </div>
         <div class="el-message-box__btns">
           <el-button
-            :loading="cancelButtonLoading"
-            :class="[ cancelButtonClasses ]"
-            v-show="showCancelButton"
-            @click.native="handleAction('cancel')">
-            {{ cancelButtonText || t('el.messagebox.cancel') }}
-          </el-button>
-          <el-button
             :loading="confirmButtonLoading"
             ref="confirm"
             :class="[ confirmButtonClasses ]"
             v-show="showConfirmButton"
             @click.native="handleAction('confirm')">
             {{ confirmButtonText || t('el.messagebox.confirm') }}
+          </el-button>
+          <el-button
+            :loading="cancelButtonLoading"
+            :class="[ cancelButtonClasses ]"
+            v-show="showCancelButton"
+            @click.native="handleAction('cancel')">
+            {{ cancelButtonText || t('el.messagebox.cancel') }}
           </el-button>
         </div>
       </div>
